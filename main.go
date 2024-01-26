c341c8fac114b4a55324cbe59bc8add7d9cf41d4
maxtheturtle0 <m.andrei.antonescu@gmail.com>
2024-01-26 13:47:10 +0100

    feat: add support for reading files instead of using repl

--- main.go
+++ main.go
@@ -1,17 +1,82 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"go/interpreter/evaluator"
+	"go/interpreter/lexer"
 	"go/interpreter/repl"
+	"go/interpreter/parser"
+	"io"
 	"os"
 	"os/user"
 )
 
 func main() {
+    // Check if the user has provided a file path as an argument
+    if len(os.Args) > 2 {
+        // If the user has provided more than one argument, print a message and exit
+        fmt.Printf("Either run this program without arguments or with the a valid input file\n")
+        os.Exit(0)
+    } else if len(os.Args) == 2 { 
+        // If the user has provided a file path, run the interpreter
+
+        filePath := os.Args[1]
+        
+        // check for right file extension
+        if filePath[len(filePath)-7:] != ".monkey" {
+            fmt.Printf("Please provide a file with the .monkey extension\n")
+            os.Exit(0)
+        }
+
+        // Open the file
+        file, err := os.Open(filePath)
+        if err != nil {
+            fmt.Println("Error opening file:", err)
+            return
+        }
+        defer file.Close()
+
+        // Create a scanner to read the file line by line
+        scanner := bufio.NewScanner(file)
+
+        // Create a variable to store the file content
+        var fileContent string
+
+        // Iterate over each line and concatenate it to the fileContent string
+        for scanner.Scan() {
+            fileContent += scanner.Text() + "\n"
+        } 
+
+        // Check for scanner errors
+        if err := scanner.Err(); err != nil {
+            fmt.Println("Error reading file:", err)
+            return
+        }
+
+        l := lexer.New(fileContent)
+        p := parser.New(l)
+
+        program := p.ParseProgram()
+
+        if len(p.Errors()) != 0 {
+            p.PrintParserErrors(os.Stdout) 
+        }
+
+        evaluated := evaluator.Eval(program)
+        if evaluated != nil {
+            io.WriteString(os.Stdout, evaluated.Inspect())
+            io.WriteString(os.Stdout, "\n")
+        }
+        os.Exit(0)
+    }     
+
+    // If the user has not provided any arguments, start the REPL
     user, err := user.Current()
     if err != nil {
         panic(err)
     }
+
     fmt.Printf("Hello %s! This is a C-like language!\n", user.Username)
     fmt.Printf("Feel free to type in commands\n")
     repl.Start(os.Stdin, os.Stdout)
